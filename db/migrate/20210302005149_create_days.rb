5309aae2ffa64c67f9b1a1be6ca9fc7dc6a71877
jrogers-dev <JRogers.dev@gmail.com>
2021-03-02 11:51:02 -0800

    Generated serializers for models Day, Meal and Food. Modified database schema to include a date column for table Days.

--- db/migrate/20210302005149_create_days.rb
+++ db/migrate/20210302005149_create_days.rb
@@ -2,6 +2,7 @@ class CreateDays < ActiveRecord::Migration[6.1]
   def change
     create_table :days do |t|
       t.references :user, null: false, foreign_key: true
+      t.date :date
 
       t.timestamps
     end
